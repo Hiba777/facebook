1e9cc3800416b5445a22a93aead351fac6d3e4d7
Hiba777 <97984505+Hiba777@users.noreply.github.com>
2022-01-21 21:39:14 +0200

    Add files via upload

---
+++ sign_handel.php
@@ -0,0 +1,16 @@
+<?php
+
+$first_name=$_POST['first-name'];
+$last_name=$_POST['last-name'];
+$email_mobile=$_POST['email-mobile'];
+$up_password=$_POST['up-password'];
+$birth_day=$_POST['birth-day'];
+$birth_month=$_POST['birth-month'];
+$birth_year=$_POST['birth-year'];
+$gen=$_POST['gen'];
+
+echo "first name is $first_name and last name is $last_name and mobile or email is $email_mobile
+and password is $up_password and birthday is $birth_day and birthmonth is $birth_month
+and birthyear is $birth_year and gender is $gen";
+
+?>
